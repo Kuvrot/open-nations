87b83ec6523dff98011465eaad7597fca592c007
Kuvrot <jamessanz@outlook.com>
2025-04-20 12:58:06 -0600

    initial commit

---
+++ IronNations.Game/Battle/Core/UnitStats.cs
@@ -0,0 +1,69 @@
+﻿using Stride.Engine;
+using Stride.Audio;
+using Stride.Particles;
+using System.Diagnostics;
+using Stride.Core.Mathematics;
+
+namespace IronNations.Battle.Core
+{
+    public class UnitStats : SyncScript
+    {
+        public bool isSelected = false;
+
+        public byte idUnit = 0; // Type of unit
+        
+        //stats
+        public short health = 450; // Number of units
+        public float damage = 20;
+        public float attackRange = 20;
+        public float speedMovement = 0.5f;
+        public float attackRate = 2; // in seconds
+        public bool isEliteUnit = false;
+        public bool canMelee = true; // if the unit can melee attack or not
+
+        //Effects
+        public Sound atackSound; //Hit sound, shoot sound, cast sound, etc.
+        public ParticleSystem attackEffect; //Shoot effect, smoke, etc.
+        public EntityComponent projectile; //(if is a range unit) Bullets, arrows, magic, etc.
+
+        //Components
+        SpriteComponent spriteUnit;
+
+        public override void Start()
+        {
+            // if is an elite unit, the stats will be improved
+            /*
+                damage + 25%
+                speed  + 25%
+                attackRate 25% faster
+                attack range + 25%
+            */
+            if (isEliteUnit)
+            {
+                damage *= 1.5f;
+                speedMovement *= 1.25f;
+                attackRate *= 0.75f;
+                attackRange *= 1.25f;
+            }
+
+            checkComponents();
+        }
+
+        public override void Update()
+        {
+            // Do stuff every new frame
+        }
+
+        public void checkComponents()
+        {
+            bool incompleteComponents = false;
+            if (spriteUnit != null)
+                incompleteComponents = true;
+
+            if (incompleteComponents)
+            {
+                DebugText.Print(Entity.Name + " has null components!!", new Int2(500, 300));
+            }
+        }
+    }
+}
