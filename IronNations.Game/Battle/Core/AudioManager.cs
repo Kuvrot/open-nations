f0cc9ee36b5f03f771a4f010931cc6e2148155a2
Kuvrot <jamessanz@outlook.com>
2025-04-20 22:20:44 -0600

    implemented audio

---
+++ IronNations.Game/Battle/Core/AudioManager.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Stride.Core.Mathematics;
+using Stride.Input;
+using Stride.Engine;
+using Stride.Audio;
+
+namespace IronNations.Battle.Core
+{
+    public class AudioManager : SyncScript
+    {
+        public float volume = 1;
+
+        public override void Start()
+        {
+
+        }
+
+        public override void Update()
+        {
+            // Do stuff every new frame
+        }
+
+        public void PlaySound(Sound sound)
+        {
+            if (sound != null)
+            {
+                SoundInstance soundInstance = sound.CreateInstance();
+                soundInstance.Volume = volume;
+                soundInstance.Play();
+            }
+        }
+
+        public async void PlaySoundOnce(Sound sound)
+        {
+            if (sound != null)
+            {
+                SoundInstance soundInstance = sound.CreateInstance();
+                soundInstance.Volume = volume;
+                await soundInstance.ReadyToPlay();
+                soundInstance.Play();
+            }
+        }
+    }
+}
--- IronNations.Game/Battle/Core/UnitController.cs
+++ IronNations.Game/Battle/Core/UnitController.cs
@@ -9,6 +9,9 @@ using Stride.Engine;
 using System.Printing;
 using ServiceWire;
 using Stride.Physics;
+using System.Security.RightsManagement;
+using Stride.Audio;
+using Silk.NET.SDL;
 
 namespace IronNations.Battle.Core
 {
@@ -21,13 +24,25 @@ namespace IronNations.Battle.Core
 
         private CharacterComponent characterComponent;
         private UnitStats unitStats;
+        private AudioManager audioManager;
+        private float clock = 0;
+
+        public bool isAttacking = false;
+        public bool moving = true;
+        SoundInstance soundInstance;
 
         public override void Start()
         {
             characterComponent = Entity.Get<CharacterComponent>();
             unitStats = Entity.Get<UnitStats>();
+            audioManager = Entity.Get<AudioManager>();
+
+            clock = new Random().NextSingle();
+
+            soundInstance = unitStats.moveSound.CreateInstance();
         }
 
+        private float auxRange = 0;
         public override void Update()
         {
             if (target != null)
@@ -35,25 +50,80 @@ namespace IronNations.Battle.Core
                 LookTarget();
 
                 float distance = Vector3.Distance(Entity.Transform.Position , target.Position);
+                
                 DebugText.Print(distance.ToString(), new Int2(500, 300));
-                if (distance >= unitStats.attackRange)
+
+                if (isAttacking)
+                {
+                    auxRange = unitStats.attackRange;
+                }
+                else
+                {
+                    auxRange = 0.5f;
+                }
+
+                if (distance >= auxRange)
                 {
                     MoveToTarget(target);
                 }
                 else
                 {
+                    if (Counter() && isAttacking)
+                    {
+                        unitStats.attackEffect.ParticleSystem.Stop();
+                        unitStats.attackEffect.ParticleSystem.Play();
+                        
+                        if (distance <= auxRange / 2)
+                        {
+                            target.Entity.Get<UnitStats>().health -= unitStats.damage;
+                        }
+                        else
+                        {
+                            target.Entity.Get<UnitStats>().health -= unitStats.damage * 2;
+                        }
+
+                        audioManager.PlaySoundOnce(unitStats.attackSound);
+
+                        if (target.Entity.Get<UnitStats>().health <= 0)
+                        {
+                            target = null;
+                        }
+                    }
                     StopMoving();
                 }
-
                 LookTarget();
             }
-        }
+            else
+            {
+                if (unitStats.isEnemy)
+                {
+                    if (BattleManager.Instance.Player1Units.Count > 0)
+                    {
+                        isAttacking = true;
+                        int random = new Random().Next(0 , BattleManager.Instance.Player1Units.Count);
+                        target = BattleManager.Instance.Player1Units[random].Entity.Transform;
+                    }
+                }
+            }
+        
+            //Kill unit
+            if (unitStats.health <= 0)
+            {
+                StopMoving();
+                characterComponent.Enabled = false;
+                target = null;
 
+            }
+        
+        }
         public void LookTarget()
         {
-            Vector2 lookAngle = GetLookAtAngle(Entity.Transform.Position, target.Position);
-            Quaternion result = Quaternion.RotationYawPitchRoll(lookAngle.Y, 0, 0);
-            Entity.Transform.Rotation = result;
+           if (target != null)
+            {
+                Vector2 lookAngle = GetLookAtAngle(Entity.Transform.Position, target.Position);
+                Quaternion result = Quaternion.RotationYawPitchRoll(lookAngle.Y, 0, 0);
+                Entity.Transform.Rotation = result;
+            }
         }
 
         private Vector2 GetLookAtAngle(Vector3 source, Vector3 destination)
@@ -74,6 +144,19 @@ namespace IronNations.Battle.Core
         public void StopMoving()
         {
             characterComponent.SetVelocity(Vector3.Zero);
+        }
+
+        private bool Counter()
+        {
+            if (clock >= unitStats.attackRate)
+            {
+                clock = 0;
+                return true;
+            }
+
+            clock += 1 * (float)Game.UpdateTime.Elapsed.TotalSeconds;
+
+            return false;
         }
     }
 }
